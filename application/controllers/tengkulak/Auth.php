50b4679660cdac6a4e81c37b7f24fd7e4d1ad5c1
fikrirbb <fikrirbb@gmail.com>
2019-03-13 14:13:45 +0700

    (#) fixing login on tengkulak

--- application/controllers/tengkulak/Auth.php
+++ application/controllers/tengkulak/Auth.php
@@ -16,52 +16,52 @@ class Auth extends CI_Controller
         if($this->session->userdata('status') == "login"){
 			redirect(site_url("tengkulak/home"));
         }
-        
+
         $product = $this->product_model;
         $validation = $this->form_validation;
         $validation->set_rules($product->ruleslogin());
 
         if ($validation->run()==TRUE) {
-            $this->session->set_flashdata('gagal', 'Username atau Password Salah!'); 
+            $this->session->set_flashdata('gagal', 'Username atau Password Salah!');
             $post = $this->input->post();
             // var_dump($post);
-            $wuname = array('tkl_uname' => $post['uname']); 
-            $wpass = array('tkl_password' => md5($post['password'])); 
+            $wuname = array('tkl_uname' => $post['uname']);
+            $wpass = array('tkl_password' => $post['password']);
 
-            $login = $product->cek_user('tengkulak', $wuname, $wpass); 
+            $login = $product->cek_user('tengkulak', $wuname, $wpass);
 
-            if (!empty($login)) { 
+            if (!empty($login)) {
                 // login berhasil
                 $data_session = array(
                     'id' => $login["tkl_id"],
                     'nama' => $login["tkl_nama"],
                     'status' => "login_tengkulak"
-                    ); 
-                $this->session->set_userdata($data_session); 
-                redirect(site_url("tengkulak/homepage")); 
-            } else { 
-                // login gagal 
-                $this->session->set_flashdata('gagal', 'Username atau Password Salah!'); 
-                // redirect(site_url("tengkulak/auth")); 
+                    );
+                $this->session->set_userdata($data_session);
+                redirect(site_url("tengkulak/home"));
+            } else {
+                // login gagal
+                $this->session->set_flashdata('gagal', 'Username atau Password Salah!');
+                // redirect(site_url("tengkulak/auth"));
             }
         }
         $this->load->view("tengkulak/login");
     }
-    
+
     function logout(){
             $this->session->sess_destroy();
             redirect(site_url("tengkulak/auth"));
     }
 public function register()
-{   
-	
+{
+
     $product = $this->product_model;
     $validation = $this->form_validation;
     // $data["categori"] = $this->product_model->getAll('categori');
     $validation->set_rules($product->rulesregister());
 
     if ($validation->run()) {
-        
+
         $post = $this->input->post();
         $databarang = array(
             'tkl_nama'       => $post['name'],
@@ -69,7 +69,7 @@ public function register()
             'tkl_nohp'       => $post['nohp'],
             'tkl_uname'      => $post['uname'],
             'tkl_password'   => md5($post['password']),
-            'tkl_gender'     => $post['gender'], 
+            'tkl_gender'     => $post['gender'],
             'tkl_status'     => 'belum verifikasi'
         );
 
@@ -78,6 +78,6 @@ public function register()
         $this->session->set_flashdata('success', 'Berhasil disimpan');
     }
     $this->load->view('tengkulak/register');
-    
+
 }
 }
